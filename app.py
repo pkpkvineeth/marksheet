5995f43bf04dac52714464b4664d96ccc262cad9
pkpkvineeth <pkpkvineeth@gmail.com>
2018-11-18 15:51:58 +0530

    3.51 p.m 18/11

--- app.py
+++ app.py
@@ -51,6 +51,10 @@ def result():
         sub4_mark=request.form['inp15']
         sub4_total=request.form['inp16']
         sub4_grade=grade(sub4_mark,sub4_total)
-        return render_template('marksheet.html',name=name,reg=reg,sem=sem,college=college,sub1_name=sub1_name,sub1_mark=sub1_mark,sub1_total=sub1_total,sub2_name=sub2_name,sub2_mark=sub2_mark,sub2_total=sub2_total,sub3_name=sub3_name,sub3_mark=sub3_mark,sub3_total=sub3_total,sub4_name=sub4_name,sub4_mark=sub4_mark,sub4_total=sub4_total,sub1_grade=sub1_grade,sub2_grade=sub2_grade,sub3_grade=sub3_grade,sub4_grade=sub4_grade)
+        if(sub1_grade=="FAIL" or sub2_grade=="FAIL" or sub3_grade=="FAIL" or sub4_grade=="FAIL"):
+            status="FAILED"
+        else:
+            status="PASSED"
+        return render_template('marksheet.html',name=name,reg=reg,sem=sem,college=college,sub1_name=sub1_name,sub1_mark=sub1_mark,sub1_total=sub1_total,sub2_name=sub2_name,sub2_mark=sub2_mark,sub2_total=sub2_total,sub3_name=sub3_name,sub3_mark=sub3_mark,sub3_total=sub3_total,sub4_name=sub4_name,sub4_mark=sub4_mark,sub4_total=sub4_total,sub1_grade=sub1_grade,sub2_grade=sub2_grade,sub3_grade=sub3_grade,sub4_grade=sub4_grade,status=status)
 if(__name__=='__main__'):
     app.run()    
